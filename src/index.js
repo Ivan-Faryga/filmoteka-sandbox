d8a9b2b2885a7607c4baa1623f5f68cc7f4f382a
Ivan Faryga <ifaryga@gmail.com>
2021-09-05 22:51:13 +0300

    commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './sass/main.scss';
+
+const refs = {
+    input: documet.querySelector(".js-search-control"),
+}
+
+refs.input.addEventListener("input", onInput);
+
+function onInput(e) {
+    
+}
+
+function createGalleryMarkup(params) {
+    
+}
