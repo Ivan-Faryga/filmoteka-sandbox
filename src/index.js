ebe8ba1be93dbbce8a0d988eb158372ecb8b6db6
Ivan Faryga <ifaryga@gmail.com>
2021-09-10 01:11:44 +0300

    fixed render tranding movies

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
 import './sass/main.scss';
 import galleryMovieTpl from './templates/gallery-movie.hbs';
-
 import { getTrandingMovies } from './js/movieAPI';
 
-console.log(getTrandingMovies);
+// console.log(getTrandingMovies);
 
 // documet.addEventListener('DOMContentLoaded', () => );
-getTrandingMovies()
+// getTrandingMovies();
 
-const refs = {
-  input: documet.querySelector('.js-search-control'),
-};
+// const refs = {
+//   input: documet.querySelector('.js-search-control'),
+// };
 
-refs.input.addEventListener('input', onInput);
+document.addEventListener('DOMContentLoaded', () => {
+  renderTrending(1);
+});
+
+async function renderTrending(page = 1) {
+  // refs.movieGallerySection.dataset.page = 'trending';
+  try {
+    // if (page === 1) {
+    //   refs.galleryList.innerHTML = '';
+    // }
+    const trends = await getTrandingMovies(page).then(data => {
+      console.log(data.results);
+      return data.results;
+    });
+
+    createGalleryMarkup(trends);
+  } catch (e) {
+    console.log('this is error:', e);
+  }
+}
+
+// refs.input.addEventListener('input', onInput);
 
 function onInput(e) {}
 
@@ -21,11 +41,3 @@ function createGalleryMarkup(params) {
   const gallery = document.querySelector('.js-gallery-list');
   gallery.insertAdjacentHTML('beforeend', markup);
 }
-
-// async function getMovies() {
-//   await getTrandingMovies().then(data => createGalleryMarkup(data));
-// }
-
-// getMovies();
-
-getTrandingMovies();
--- src/js/movieAPI.js
+++ src/js/movieAPI.js
@@ -2,14 +2,19 @@ import axios from 'axios';
 
 const BASE_URL = 'https://image.tmdb.org/t/p/';
 const API_KEY = '22c199c70cf21c77ac06b5d74d444545';
+const language = 'en-US';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
 
 async function getTrandingMovies(page = 1) {
-  const response = await axios
-    .get(`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&page=${page}`);
-    // .then(response => console.log(response.data))
+  const response = await axios.get(
+    `trending/movie/day?api_key=${API_KEY}&language=${language}&page=${page}`,
+  );
+  // const response = await axios.get(
+  //   `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&page=${page}`,
+  // );
+  // .then(response => console.log(response.data))
   console.log(response.data);
   return await response.data;
-  
 }
 
 export { getTrandingMovies };
